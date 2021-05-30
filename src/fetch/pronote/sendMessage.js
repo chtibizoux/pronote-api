02d9a74234b86db59cca70433c4ed5ca796250e1
Clément <chtibizoux@users.noreply.github.com>
2021-05-30 15:13:17 +0200

    modify send message

--- src/fetch/pronote/sendMessage.js
+++ src/fetch/pronote/sendMessage.js
@@ -7,7 +7,7 @@ const PAGE_NAME = 'SaisieMessage';
 const TAB_ID = 131;
 const ACCOUNTS = ['student', 'parent'];
 // eslint-disable-next-line max-len
-async function setSendMessage(session, user, object = '', content = '', files = [], responceMessage = {}, private = false)
+async function setSendMessage(session, user, responceMessage = {}, content = '', private = false, object = '', files = [])
 {
     const message = await navigate(session, user, PAGE_NAME, TAB_ID, ACCOUNTS, {
         bouton: {
