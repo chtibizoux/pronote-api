a3a21f3586109d4f48708ee921648c8719568b12
Clément <chtibizoux@users.noreply.github.com>
2021-09-08 17:10:19 +0200

    add wording

--- src/fetch/pronote/communicationResources.js
+++ src/fetch/pronote/communicationResources.js
@@ -5,12 +5,12 @@ const PAGE_NAME = 'ListeRessourcesPourCommunication';
 const TAB_ID = 7;
 const ACCOUNTS = ['student', 'parent'];
 
-async function getCommunicationResources(session, user)
+async function getCommunicationResources(session, user, wording)
 {
     const resources = await navigate(session, user, PAGE_NAME, TAB_ID, ACCOUNTS, {
         onglet: {
             N: 0,
-            G: 3
+            G: wording === 'Professeurs' ? 3 : 34
         },
         filtreElement: {
             N: 0,
